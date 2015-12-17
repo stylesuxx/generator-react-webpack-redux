f9ea6a7b8d5868142ee9fc2fe2f69d5b63e048f0
stylesuxx <stylesuxx@gmail.com>
2015-12-17 18:48:34 +0100

    Generated base and constant name

--- generators/action/index.js
+++ generators/action/index.js
@@ -1,6 +1,7 @@
 'use strict';
 let generator = require('yeoman-generator');
 let utils = require('generator-react-webpack/utils/yeoman');
+let path = require('path');
 
 module.exports = generator.NamedBase.extend({
 
@@ -10,14 +11,16 @@ module.exports = generator.NamedBase.extend({
 
   writing: function() {
 
-    let destinationPath = utils.getDestinationPath(this.name, 'action', 'Actions');
-    let baseName = utils.getDestinationClassName(this.name, 'action', 'Actions');
+    let baseName = this.name.trim();
+    let destinationPath = path.join('src', 'actions', baseName + '.js')
+    let constantName = (baseName.split(/(?=[A-Z])/).join('_')).toUpperCase();
 
     this.fs.copyTpl(
       this.templatePath('Action.js'),
       this.destinationPath(destinationPath),
       {
-        actionClass: baseName
+        actionName: baseName,
+        actionConstant: constantName
       }
     );
   }
