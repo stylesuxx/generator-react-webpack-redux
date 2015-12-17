983a227c98469d9d6a374175427b3d4d042ac615
stylesuxx <stylesuxx@gmail.com>
2015-12-17 23:50:00 +0100

    Use App container instead of Main component

--- generators/root/templates/run.js
+++ generators/root/templates/run.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from '../stores';
-import App from './Main';
+import App from '../containers/App';
 
 render(
   <Provider store={store}>
