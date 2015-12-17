285e5b4f29da336aac1ab57e0121c7e8efeed186
stylesuxx <stylesuxx@gmail.com>
2015-12-17 12:44:41 +0100

    Default case should be the only enabled

--- generators/reducer/templates/reducer.js
+++ generators/reducer/templates/reducer.js
@@ -6,10 +6,12 @@ const <%= reducerName %> = function (state = initialState, action) {
   var nextState = Object.assign({}, state);
 
   switch (action.type) {
+    /*
     case 'YOUR_ACTION': {
       // Modify next state depending on the action and return it
       return nextState;
     } break;
+    */
     default: {
       return state;
     }
