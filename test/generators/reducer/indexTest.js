137941736163700196d2a4959153b1d2dba86db2
stylesuxx <stylesuxx@gmail.com>
2015-12-18 01:42:41 +0100

    Check that the reducer is attached to the root reducer and the App container state is updated accordingly.

--- test/generators/reducer/indexTest.js
+++ test/generators/reducer/indexTest.js
@@ -1,13 +1,18 @@
 'use strict';
 let path = require('path');
 let assert = require('yeoman-generator').assert;
-let helpers = require('yeoman-generator').test
+let helpers = require('yeoman-generator').test;
+let fs = require('fs-extra');
 
 describe('react-webpack-redux:reducer', () => {
 
   describe('When creating a new reducer', () => {
 
-    let generatorReducer = path.join(__dirname, '../../../generators/reducer');
+    const generatorReducer = path.join(__dirname, '../../../generators/reducer');
+    const reducerSource = path.join(__dirname, '../../../generators/root/templates/reducer.js');
+    const appSource = path.join(__dirname, '../../../generators/root/templates/App.js');
+    let rootReducerPath = '';
+    let appPath = '';
 
     /**
      * Return a newly generated reducer with given name
@@ -16,6 +21,13 @@ describe('react-webpack-redux:reducer', () => {
      */
     function createGeneratedReducer(name, callback) {
       helpers.run(generatorReducer)
+        .inTmpDir(function(tmpDir) {
+          rootReducerPath = path.join(tmpDir, 'src/reducers/index.js');
+          fs.copySync(reducerSource, rootReducerPath);
+
+          appPath = path.join(tmpDir, 'src/containers/App.js');
+          fs.copySync(appSource, appPath);
+        })
         .withArguments([name])
         .on('end', callback);
     }
@@ -38,6 +50,22 @@ describe('react-webpack-redux:reducer', () => {
           'test/reducers/testTest.js'
         ]);
 
+        done();
+      });
+    });
+
+    it('should add the reducer to the root reducer', (done) => {
+
+      createGeneratedReducer('test', () => {
+        assert.fileContent(rootReducerPath, 'test: require(\'./test.js\')');
+        done();
+      });
+    });
+
+    it('should add the reducer to App.js', (done) => {
+
+      createGeneratedReducer('test', () => {
+        assert.fileContent(appPath, 'test: state.test');
         done();
       });
     });
