4ef978bb9b965947c152661d65abdf15ea88e336
stylesuxx <stylesuxx@gmail.com>
2015-12-19 20:11:43 +0100

    Update the root reducer when creating a new reducer. Update the main Application when a new reducer is added

--- generators/reducer/index.js
+++ generators/reducer/index.js
@@ -1,33 +1,145 @@
 'use strict';
-let esprima = require('esprima');
-let walk = require('esprima-walk');
-let escodegen = require('escodegen');
 let generator = require('yeoman-generator');
-//let utils = require('generator-react-webpack/utils/all');
 let path = require('path');
 let fs = require('fs');
+let walk = require('esprima-walk');
+
+/* For regular JS files */
+//let esprima = require('esprima');
+//let escodegen = require('escodegen');
+
+/* For files with JSX syntax */
+let esprimaFb = require('esprima-fb');
+let escodegenJsx = require('escodegen-wallaby');
 
 module.exports = generator.NamedBase.extend({
-
   constructor: function() {
     generator.NamedBase.apply(this, arguments);
+
+    this.attachToRoot = function(path, name) {
+      const reducerPath = ['./', name, '.js'].join('');
+      const reducerNode = {
+        type: 'Property',
+        kind: 'init',
+        key: { type: 'Identifier', name: name },
+        value: {
+          type: 'CallExpression',
+          callee: { type: 'Identifier', name: 'require' },
+          arguments: [ { type: 'Literal', value: reducerPath } ]
+        }
+      };
+
+      let tree = this.read(path);
+      walk(tree, function(node) {
+        if(node.type === 'VariableDeclarator' && node.id.name === 'reducers') {
+          node.init.properties.push(reducerNode);
+        }
+      });
+
+      this.write(path, tree);
+    };
+
+    this.attachToApp = function(path, name) {
+      const stateNode = {
+        type: 'Property',
+        kind: 'init',
+        key: { type: 'Identifier', name: name },
+        value: {
+          type: 'MemberExpression',
+          object: { type: 'Identifier', name: 'state' },
+          property: { type: 'Identifier', name: name }
+        }
+      };
+
+      let tree = this.read(path);
+      walk(tree, function(node) {
+        // Map reducer to state props
+        if(node.type === 'VariableDeclarator' && node.id.name === 'props') {
+          node.init.properties.push(stateNode);
+        }
+
+        // Add state to main container
+        if(node.type === 'MethodDefinition' && node.key.name === 'render') {
+          const diff = {
+            value: {
+              type: 'Identifier',
+              name: name,
+              typeAnnotation: undefined,
+              optional: undefined
+            },
+            shorthand: true
+          }
+          const propNode = Object.assign({}, stateNode, diff);
+          node.value.body.body[0].declarations[0].id.properties.push(propNode);
+        }
+
+        if(node.type === 'MethodDefinition' && node.key.name === 'render') {
+          const attribute = {
+            type: 'JSXAttribute',
+            name: { type: 'JSXIdentifier', name: name},
+            value: {
+              type: 'JSXExpressionContainer',
+              expression: {
+                type: 'Identifier',
+                name: name
+              }
+            }
+          }
+          node.value.body.body[1].argument.openingElement.attributes.push(attribute);
+        }
+
+        // Make the reducers state required
+        if(node.type === 'AssignmentExpression' && node.left.object.name === 'App') {
+          const diff = {
+            value: {
+              type: 'MemberExpression',
+              object: { type: 'Identifier', name: 'PropTypes' },
+              property: { type: 'Identifier', name: 'object.isRequired' }
+            }
+          }
+          const propNode = Object.assign({}, stateNode, diff);
+          node.right.properties.push(propNode);
+        }
+      });
+
+      this.write(path, tree);
+    };
+
+    this.read = function(path) {
+      const data = fs.readFileSync(path, 'utf8');
+      const options = {
+        sourceType: 'module',
+        range: true,
+        tokens: true,
+        comment: true
+      };
+
+      return esprimaFb.parse(data, options);
+    };
+
+    this.write = function(path, tree) {
+      tree = escodegenJsx.attachComments(tree, tree.comments, tree.tokens);
+      let options = { comment: true, format: { indent: { style: '  ' } } };
+      let code = escodegenJsx.generate(tree, options);
+      fs.writeFileSync(path, code, 'utf8');
+    };
   },
 
   writing: function() {
-
     let baseName = this.name;
     let destinationPath = path.join('src', 'reducers', baseName + '.js');
     let rootReducerPath = this.destinationPath(path.join('src', 'reducers', 'index.js'));
+    let appPath = this.destinationPath(path.join('src', 'containers', 'App.js'));
     let testDestinationPath = path.join('test', 'reducers', baseName + 'Test.js');
 
-    // Copy the base store
+    // Copy the reducer template
     this.fs.copyTpl(
       this.templatePath('reducer.js'),
       this.destinationPath(destinationPath),
       { reducerName: baseName }
     );
 
-    // Copy the unit test
+    // Copy the reducers unit test
     this.fs.copyTpl(
       this.templatePath('Test.js'),
       this.destinationPath(testDestinationPath),
@@ -35,23 +147,9 @@ module.exports = generator.NamedBase.extend({
     );
 
     // Add the reducer to the root reducer
-    let reducer = 'var reducer = { ' + baseName +': require(\'./' + baseName + '.js\')}';
-    reducer = esprima.parse(reducer);
-    reducer = reducer.body[0].declarations[0].init.properties[0];
+    this.attachToRoot(rootReducerPath, baseName);
 
-    let data = fs.readFileSync(rootReducerPath, 'utf8');
-    let parsed = esprima.parse(data);
-
-    walk.walkAddParent(parsed, function(node) {
-      if(node.type === 'VariableDeclarator' && node.id.name === 'reducers') {
-        node.init.properties.push(reducer);
-      }
-    });
-
-    let options = { format: { indent: { style: '  ' } } };
-    let code = escodegen.generate(parsed, options);
-    fs.writeFileSync(rootReducerPath, code, 'utf8');
-
-    // TODO: Add the reducer to App.js
+    // Add the reducer to App.js
+    this.attachToApp(appPath, baseName);
   }
 });
