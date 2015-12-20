437fc2524f8da4cdf5cb95e99c2c37bc1301cccf
stylesuxx <stylesuxx@gmail.com>
2015-12-20 17:42:05 +0100

    Comment out nextState to supress warning

--- generators/reducer/templates/reducer.js
+++ generators/reducer/templates/reducer.js
@@ -1,9 +1,9 @@
 const initialState = {
   /* Define your initial state here */
-}
+};
 
 const <%= reducerName %> = function (state = initialState, action) {
-  var nextState = Object.assign({}, state);
+  //var nextState = Object.assign({}, state);
 
   switch (action.type) {
     /*
