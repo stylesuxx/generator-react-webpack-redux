8cd26aa67f9c3fa61225de3c9ee15e64c3c6defd
stylesuxx <stylesuxx@gmail.com>
2015-12-21 00:52:06 +0100

    Improved reducer template

--- generators/reducer/templates/reducer.js
+++ generators/reducer/templates/reducer.js
@@ -1,11 +1,15 @@
-const initialState = {
-  /* Define your initial state here */
-};
+/* Define your initial state here.
+ *
+ * If you change the type from object to something else, do not forget to update
+ * src/container/App.js accordingly.
+ */
+const initialState = {};
 
-const <%= reducerName %> = function (state = initialState, action) {
+module.exports = function(state = initialState, action) {
+  /* Keep the reducer clean - do not mutate the original state. */
   //var nextState = Object.assign({}, state);
 
-  switch (action.type) {
+  switch(action.type) {
     /*
     case 'YOUR_ACTION': {
       // Modify next state depending on the action and return it
@@ -13,9 +17,8 @@ const <%= reducerName %> = function (state = initialState, action) {
     } break;
     */
     default: {
+      /* Return original state if no actions were consumed. */
       return state;
     }
   }
 }
-
-module.exports = <%= reducerName %>;
