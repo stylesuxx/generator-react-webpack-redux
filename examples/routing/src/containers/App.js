37bd4dd687bc1ec1fb887c752b2e74d0e043c326
Chris Landa <stylesuxx@gmail.com>
2016-06-21 00:48:16 +0200

    Added login action

--- examples/routing/src/containers/App.js
+++ examples/routing/src/containers/App.js
@@ -23,9 +23,7 @@ class App extends Component {
  * HINT: if you adjust the initial type of your reducer, you will also have to
  *       adjust it here.
  */
-App.propTypes = {
-  actions: PropTypes.object.isRequired
-};
+App.propTypes = { actions: PropTypes.object.isRequired };
 function mapStateToProps(state) {
   /* Populated by react-webpack-redux:reducer */
   const props = {};
@@ -33,7 +31,7 @@ function mapStateToProps(state) {
 }
 function mapDispatchToProps(dispatch) {
   /* Populated by react-webpack-redux:action */
-  const actions = {};
+  const actions = { login: require('../actions/login.js') };
   const actionMap = { actions: bindActionCreators(actions, dispatch) };
   return actionMap;
 }
