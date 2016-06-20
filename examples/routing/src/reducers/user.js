649c9c09c9a66e085b33346f5062976801543423
Chris Landa <stylesuxx@gmail.com>
2016-06-21 01:10:20 +0200

    Change login state When LOGIN action was triggered.

--- examples/routing/src/reducers/user.js
+++ examples/routing/src/reducers/user.js
@@ -1,21 +1,24 @@
+import {LOGIN} from '../actions/const';
+
 /* Define your initial state here.
  *
  * If you change the type from object to something else, do not forget to update
  * src/container/App.js accordingly.
  */
-const initialState = {};
+const initialState = {
+  login: false
+};
 
 module.exports = function(state = initialState, action) {
   /* Keep the reducer clean - do not mutate the original state. */
-  //let nextState = Object.assign({}, state);
+  let nextState = Object.assign({}, state);
 
   switch(action.type) {
-    /*
-    case 'YOUR_ACTION': {
-      // Modify next state depending on the action and return it
+    case LOGIN: {
+      nextState.login = true;
       return nextState;
-    } break;
-    */
+    }
+
     default: {
       /* Return original state if no actions were consumed. */
       return state;
