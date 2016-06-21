f6bff8bd54b719f3f757ab6a5a84fd2dbcc8a798
Chris Landa <stylesuxx@gmail.com>
2016-06-21 04:09:59 +0200

    Set routes

--- examples/routing/src/components/Main.js
+++ examples/routing/src/components/Main.js
@@ -1,7 +1,16 @@
 require('normalize.css/normalize.css');
 require('styles/App.css');
 
+
 import React from 'react';
+
+import IndexRoute from '../containers/IndexRoute';
+import SelfRoute from '../containers/SelfRoute';
+
+//import IndexComponent from './IndexComponent';
+//import WelcomeComponent from './WelcomeComponent';
+
+import { Router, Route, browserHistory } from 'react-router';
 
 let yeomanImage = require('../images/yeoman.png');
 
@@ -11,6 +20,10 @@ class AppComponent extends React.Component {
       <div className="index">
         <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+        <Router history={browserHistory}>
+          <Route path='/' component={IndexRoute}></Route>
+          <Route path='/self' component={SelfRoute}></Route>
+        </Router>
       </div>
     );
   }
@@ -18,5 +31,12 @@ class AppComponent extends React.Component {
 
 AppComponent.defaultProps = {
 };
+/*
+<IndexComponent
+  login={this.props.user.login}
+  loginAction={this.props.actions.login}
+  url="somepath" />
+<WelcomeComponent login={this.props.user.login} />
+*/
 
 export default AppComponent;
