af18ab6ec5e7b6e7c41e52dba7e1db83709eebe2
Chris Landa <stylesuxx@gmail.com>
2016-06-23 01:35:31 +0200

    Template for central action export

---
+++ generators/root/templates/action/index.js
@@ -0,0 +1,9 @@
+/* Exports all the actions from a single point.
+
+Allows to import actions like so:
+
+import {action1, action2} from '../actions/'
+*/
+module.exports = {
+  /* Populated by react-webpack-redux:action */
+}
