e2529437560477fee7c1181cbd3b6a30f9e14333
Chris Landa <stylesuxx@gmail.com>
2016-09-09 19:43:21 +0200

    eslint should ignore the unused state variable, since in a fresh project, it simply is not used.

--- generators/root/templates/App.js
+++ generators/root/templates/App.js
@@ -27,7 +27,7 @@ class App extends Component {
 App.propTypes = {
   actions: PropTypes.object.isRequired
 };
-function mapStateToProps(state) {
+function mapStateToProps(state) { // eslint-disable-line no-unused-vars
   /* Populated by react-webpack-redux:reducer */
   const props = {};
   return props;
