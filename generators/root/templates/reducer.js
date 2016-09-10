301bfbaf583ac67b96778bbb6ace52dbab56c231
Chris Landa <stylesuxx@gmail.com>
2016-09-10 15:22:00 +0200

    Fixed white spacing

--- generators/root/templates/reducer.js
+++ generators/root/templates/reducer.js
@@ -8,6 +8,7 @@
  */
 /* Populated by react-webpack-redux:reducer */
 import { combineReducers } from 'redux';
-const reducers = { };
+
+const reducers = {};
 const combined = combineReducers(reducers);
 module.exports = combined;
