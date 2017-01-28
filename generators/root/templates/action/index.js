a450150a9c3e701830f370ff1ac6b229dfc43948
Chris L <stylesuxx@gmail.com>
2017-01-28 18:40:35 +0100

    disable newline after import linting rule since ast parser does not keep empty lines

--- generators/root/templates/action/index.js
+++ generators/root/templates/action/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/newline-after-import */
 /* Exports all the actions from a single point.
 
 Allows to import actions like so:
@@ -6,4 +7,4 @@ import {action1, action2} from '../actions/'
 */
 /* Populated by react-webpack-redux:action */
 const actions = {};
-module.exports = actions;
+export default actions;
