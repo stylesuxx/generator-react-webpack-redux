f44cc671ff6d0cf716a5dc2228c5a11ed7b77d88
Chris L <stylesuxx@gmail.com>
2017-01-28 20:11:52 +0100

    Updated the component generator to extend the base generator

--- generators/component/index.js
+++ generators/component/index.js
@@ -1,26 +1,20 @@
 'use strict';
 const Generator = require('yeoman-generator');
 
-module.exports = class ComponentGenerator extends Generator {
+class ComponentGeneratorWrapper extends Generator {
 
-  constructor() {
-    generator.Base.apply(this, arguments);
+  constructor(args, opts) {
+    super(args, opts);
     this.argument('name', { type: String, required: true });
   }
 
   writing() {
-    // Build options
-    let opts = {};
-
-    if(this.options.stateless === true) {
-      opts.stateless = true;
-    }
-
-    this.composeWith('react-webpack', {
-      options: opts,
-      args: [ this.options.name ]
-    }, {
-      local: require.resolve('generator-react-webpack/generators/component')
-    });
+    this.composeWith(
+      'generator-react-webpack/generators/component',
+      { options: this.options, arguments: this.arguments },
+      { local: require.resolve('generator-react-webpack') }
+    );
   }
 };
+
+module.exports = ComponentGeneratorWrapper;
